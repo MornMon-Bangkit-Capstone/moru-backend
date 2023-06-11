c3c5d618098f2e5723ab4ff4e802efd9b7945f7f
flitzcore <hemistergt@gmail.com>
2023-06-11 13:52:30 +0700

    fix: databse bug🐛

--- controllers/database.js
+++ controllers/database.js
@@ -1,19 +1,22 @@
 const pool = require('database/index');
 // GET all book from google book API
-exports.getTitle = async (req, res) => {
+exports.getTitle = (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) {
+      connection.release();
       console.error('Error connecting to database:', err);
       return res.status(500).json({message: 'Internal server error.'});
     }
     const checkBooksQuery='SELECT ISBN, BookTitle FROM books';
     connection.query(checkBooksQuery, (err, results) => {
       if (err) {
+        connection.release();
         return res.status(400).json({
           error: true,
           message: err.message,
         });
       }
+      connection.release();
       return res.status(201).json({
         error: false,
         message: results,
@@ -21,20 +24,23 @@ exports.getTitle = async (req, res) => {
     });
   });
 };
-exports.getRating = async (req, res) => {
+exports.getRating = (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) {
+      connection.release();
       console.error('Error connecting to database:', err);
       return res.status(500).json({message: 'Internal server error.'});
     }
     const checkBooksQuery='SELECT * FROM bookRating';
     connection.query(checkBooksQuery, (err, results) => {
       if (err) {
+        connection.release();
         return res.status(400).json({
           error: true,
           message: err.message,
         });
       }
+      connection.release();
       return res.status(201).json({
         error: false,
         message: results,
@@ -42,19 +48,22 @@ exports.getRating = async (req, res) => {
     });
   });
 };
-exports.customQuery = async (req, res) => {
+exports.customQuery = (req, res) => {
   const {query}=req.body;
   pool.getConnection((err, connection) => {
     if (err) {
+      connection.release();
       return res.status(500).json({message: 'Internal server error.'});
     }
     connection.query(query, (err, results) => {
       if (err) {
+        connection.release();
         return res.status(400).json({
           error: true,
           message: err.message,
         });
       }
+      connection.release();
       return res.status(201).json({
         error: false,
         message: results,
